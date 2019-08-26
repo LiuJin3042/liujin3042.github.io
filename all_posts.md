871e988996d2e341a8123b4ddd5a62d10ffe8e76
我永远爱八重樱 <liujin3042@gmail.com>
2019-08-26 14:35:17 +0800

    update

---
+++ all_posts.md
@@ -0,0 +1,5 @@
+# кЫспндуб  
+  * [Git](./source/Git/contents.md)  
+  * [Markdown](./source/Markdown/contents.md)  
+  * [Python](./source/Python/contents.md)  
+  * [ThisBlog](./source/ThisBlog/contents.md)  
--- gen_index.py
+++ gen_index.py
@@ -42,10 +42,14 @@ for sub_dir in dir_2: # 对每一个子目录遍历查找文件
     dir_3.append(parent_dir(sub_dir,file_list))
     
 index_contents = [] # index.md的文件内容
-index_head = '# MyBlog  \n## [全部文章](./source/all_posts.md)  \n## 近期文章  \n'
+index_head = '# MyBlog  \n## [全部文章](./all_posts.md)  \n## 近期文章  \n'
 index_contents.append(index_head)
-for each_dir in dir_3:
-    dir_index = [] # 每个目录的主页
+all_posts = [] # 所有子文件夹的目录
+all_posts.append('# 所有文章  \n')
+                 
+for each_dir in dir_3:             
+    all_posts.append(''.join(['  * [',each_dir.dir_name,'](',dir_1,'/',each_dir.dir_name,'/contents.md)  \n']))
+    dir_index = [] # 每个子文件夹的主页
     dir_index.append('# '+ each_dir.dir_name + '  \n')
     file_list = each_dir.file_list 
     # 给子目录主页添加内容                 
@@ -67,6 +71,7 @@ for each_dir in dir_3:
                                            each_file.day,'日: ','[',each_file.title,\
                                            '](',dir_1,'/',each_dir.dir_name,'/',\
                                            each_file.origin_title,')  \n']))
+        index_contents.append('\n') # 所有文件输出完之后还有再一次换行
     # 如果不足5条就全部显示
     else:
         for each_file in file_list:
@@ -76,11 +81,15 @@ for each_dir in dir_3:
                                            each_file.day,'日: ','[',each_file.title,\
                                            '](',dir_1,'/',each_dir.dir_name,'/',\
                                            each_file.origin_title,')  \n']))
+        index_contents.append('\n') # 所有文件输出完之后还有再一次换行
     with open(''.join([dir_1,'/',each_dir.dir_name,'/contents.md']),'w') as contents:
         contents.writelines(dir_index)
+        
 with open('./index.md','w') as index:
     index.writelines(index_contents)
-    
+
+with open('./all_posts.md','w') as posts:
+    posts.writelines(all_posts)
     
     
     
